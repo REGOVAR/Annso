845f1489f41caaa9fed4777e3249753868decc10
Jérémie Roquet <jroquet@arkanosis.net>
2017-03-10 06:34:15 +0100

    Missing dependencies and classes

--- annso/annso_server.py
+++ annso/annso_server.py
@@ -5,23 +5,19 @@ import os
 
 
 
-# Pirus package
+# Annso package
 from config import *
-
-
+from core.framework import AnnsoException
 
 # Some check before starting the web application
 if not os.path.exists(TEMPLATE_DIR):
-    raise PirusException("ERROR : Templates directory doesn't exists : " + TEMPLATE_DIR)
+    raise AnnsoException("ERROR : Templates directory doesn't exists : " + TEMPLATE_DIR)
 if not os.path.exists(CACHE_DIR):
-    raise PirusException("ERROR : Cache directory doesn't exists : " + CACHE_DIR)
+    raise AnnsoException("ERROR : Cache directory doesn't exists : " + CACHE_DIR)
 if not os.path.exists(TEMP_DIR):
-    raise PirusException("ERROR : Temp directory for file upload doesn't exists : " + TEMP_DIR)
+    raise AnnsoException("ERROR : Temp directory for file upload doesn't exists : " + TEMP_DIR)
 if not os.path.exists(FILES_DIR):
-    raise PirusException("ERROR : File directory doesn't exists : " + FILES_DIR)
-
-
-  
+    raise AnnsoException("ERROR : File directory doesn't exists : " + FILES_DIR)
 
 # Load rest of pirus application shall be done after celery init
 from aiohttp import web
@@ -32,4 +28,3 @@ from api_rest import *
 # Start the pirus server
 if __name__ == '__main__':
     web.run_app(app, host=HOST, port=PORT)
-
